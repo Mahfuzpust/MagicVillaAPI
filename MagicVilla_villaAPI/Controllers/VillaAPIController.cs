09d7455d3f2df507d91e32de2ff99e40642b235d
mahfuz khan <mahfuzpust@gmail.com>
2023-01-11 23:35:09 +0600

    end of get method

--- MagicVilla_villaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_villaAPI/Controllers/VillaAPIController.cs
@@ -17,6 +17,14 @@ namespace MagicVilla_villaAPI.Controllers
         }
 
         [HttpGet("{id:int}")]
+        //[ProducesResponseType(200)]
+        //[ProducesResponseType(400)]
+        //[ProducesResponseType(404)]
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
         public ActionResult <VillaDTO> GetVilla(int id)
         {
             if (id == 0)
